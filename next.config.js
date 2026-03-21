710f04b73ca9c3b4ace267ac2a81ab3189111192
daksh <lakhotiyadk02@gmail.com>
2026-03-21 12:14:54 +0530

    chore: enable static export for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Uncomment the lines below to enable static export for GitHub Pages deployment:
-  // output: 'export',
-  // basePath: '/Daksh-Lakhotiya',
+  output: 'export',
+  basePath: '/Daksh-Lakhotiya',
 };
 
 module.exports = nextConfig;
